8fb932475620efcef0b4ce46349ebc716755ab75
dernn <fajj@mail.ru>
2024-05-06 16:17:16 +0600

    board app + models

---
+++ board/models.py
@@ -0,0 +1,47 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import html
+
+
+# define AUTH_USER_MODEL in settings
+class User(AbstractUser):
+    code = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    subscriber = models.ManyToManyField(User, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60, default='Title')
+    text = RichTextUploadingField()
+
+    def __str__(self):
+        return f'{self.title}: {self.text[:40]}'
+
+    def preview(self):
+        result = html.strip_tags(self.text)
+        return f'{result[:40]}...'
+
+    def get_absolute_url(self):
+        return f'/post/{self.id}'
+
+
+class Comment(models.Model):
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    accept = models.BooleanField(default=False)
