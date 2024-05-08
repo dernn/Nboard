69e525a5d710de5f3d3d385373aba36523fbcc70
dernn <fajj@mail.ru>
2024-05-08 12:10:31 +0600

    [board] migrations + db create

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,8 @@
+from board.models import Category, Comment, Post, User
+
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comment)
