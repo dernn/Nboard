59100ceba27d3aac5bf890e38dfaa714eefe8cdd
dernn <fajj@mail.ru>
2024-05-10 15:56:37 +0600

    models.Post disabled ckeditor [temp]

--- board/models.py
+++ board/models.py
@@ -2,6 +2,7 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import TextField
 from django.utils import html
 
 
@@ -26,7 +27,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=60, default='Title')
-    text = RichTextUploadingField()
+    text = TextField()
 
     def __str__(self):
         return f'{self.title}: {self.text[:40]}'
