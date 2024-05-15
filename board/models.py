bd0f2b13566abd981df78cd85430a7e50135190c
dernn <fajj@mail.ru>
2024-05-15 14:56:27 +0600

    models.Category fixture + m2m relation fix

--- board/models.py
+++ board/models.py
@@ -16,7 +16,6 @@ class User(AbstractUser):
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    subscriber = models.ManyToManyField(User, related_name='categories')
 
     def __str__(self):
         return self.name
